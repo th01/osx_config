fe97abd27f13a168d2e1a86436d74f4fe5eb410f
Travis Hooper <thooper-dt@Traviss-Mac-Pro.local>
2015-03-28 01:02:57 -0500

    Adds cmd-y keybinding to terminal

--- zsh_profiles/bindkey.zsh
+++ zsh_profiles/bindkey.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-bindkey "^[[1;9a" backward-kill-line
-bindkey "^[[1;9b" kill-line
-bindkey "^[[1;9c" kill-word
+bindkey "\e[1;9a" backward-kill-line
+bindkey "\e[1;9b" kill-line
+bindkey "\e[1;9c" kill-word
+bindkey "\e[1;9d" undo
+bindkey "\e[1;9e" redo
